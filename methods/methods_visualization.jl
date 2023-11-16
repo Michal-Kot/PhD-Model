49d3c37408bb8808a0c7752f03d32e32e01c3a7f
Michal Kot <mihau.kot@gmail.com>
2023-11-16 21:48:43 +0100

    Update methods_visualization.jl

--- methods/methods_visualization.jl
+++ methods/methods_visualization.jl
@@ -4,7 +4,7 @@
 
 #######################################################################################################
 
-function plot_ecdf(is_new, metric, label; legend = :bottomright, xlabel="", ylabel="", title="", xlim=nothing, ylim=nothing)
+function plot_ecdf(is_new, metric, label; legend = :bottomright, xlabel="", ylabel="", title="", xlim=nothing, ylim=nothing, legendfontsize = 8)
 
     """
     Prints ECDF chart
@@ -17,7 +17,7 @@ function plot_ecdf(is_new, metric, label; legend = :bottomright, xlabel="", ylab
     """
 
     if is_new
-        Plots.plot(sort(metric), (1:length(metric))./length(metric), xlabel = xlabel, ylabel = ylabel, title = title, label = label, legend=legend, legendfontsize = 6, xlim=xlim, ylim=ylim)
+        Plots.plot(sort(metric), (1:length(metric))./length(metric), xlabel = xlabel, ylabel = ylabel, title = title, label = label, legend=legend, xlim=xlim, ylim=ylim, legendfontsize = legendfontsize)
     else
         Plots.plot!(sort(metric), (1:length(metric))./length(metric), label = label)
     end
